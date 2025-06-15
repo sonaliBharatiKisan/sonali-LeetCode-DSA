8454e9543ae8a1165933145a2c96a5a5c33926c1
sonaliBharatiKisan <sonali.bharati@cumminscollege.in>
2025-06-15 16:02:11 +0530

    Palindrome checking

---
+++ palindrome.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev=0;
+        int temp=x;
+        while(temp!=0){
+            int ld=temp%10;
+            rev=rev*10+ld;
+            temp=temp/10;
+
+        }
+        return (rev==x);
+        
+    }
+}
